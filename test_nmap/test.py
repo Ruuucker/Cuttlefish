93b821231d9ad3aa555d227f25e0d9214a4ac50c
Ruuucker <russ22223@gmail.com>
2020-10-16 09:37:10 -0400

    Update

---
+++ test_nmap/test.py
@@ -0,0 +1,11 @@
+from radialnet.core.XMLHandler import XMLReader
+# from radialnet.gui.RadialNet import *
+# from radialnet.gui.ControlWidget import *
+# from radialnet.gui.Toolbar import Toolbar
+# from radialnet.bestwidgets.boxes import *
+# from radialnet.bestwidgets.windows import *
+# from radialnet.util.integration import make_graph_from_hosts
+
+
+print 'test'
+
