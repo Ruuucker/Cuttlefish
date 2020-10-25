c505655e5be0f3ac8d9321ea97beae4426f7fea2
Rucker <russ22223@gmail.com>
2020-10-25 15:07:12 +0000

    Problem in express

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ const { startServer } = require('./server.js');
 
 var dirToSave = '/tmp/';
 var diaps = ['192.168.1.0/24', '192.168.1.1'];
+// var diaps = ['192.168.1.1'];
 
 var scansPromises = [];
-for (var i = diaps.length - 1; i >= 0; i--) {
+for (let i = diaps.length - 1; i >= 0; i--) {
 	let fileName = generateFileName();
 	scansPromises.push(startScan(diaps[i], dirToSave, fileName));
 }
 
 Promise.all(scansPromises).then((xmlPaths) => {
 	console.log(xmlPaths);
-	// let jsonPath = convertNmapOutput(dirToSave, xmlPath, fileName);
-	startServer(xmlPaths[0]);
-	// console.log('Иди и смотри');
+
+	let jsonPaths = [];
+
+	for (let i = xmlPaths.length - 1; i >= 0; i--) {
+		let fileName = generateFileName();
+		jsonPaths.push(convertNmapOutput(dirToSave, xmlPaths[i], fileName));
+	}
+	console.log(jsonPaths);
+	startServer(jsonPaths);
 });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-function startServer (pathToJSON) {
+function startServer (jsonPaths) {
 	app.get('/', (req, res) => {
 	    res.sendFile('test.html', {root: __dirname })
 	});
@@ -12,18 +12,23 @@ function startServer (pathToJSON) {
 	    res.sendFile('libs/VivaGraphJS/dist/vivagraph.js', {root: __dirname })
 	});
 
-	// app.get('/nmapJSONArray', (req, res) => {
-	//     res.sendFile(pathToJSON)
-	// });
-
-	app.get('/test', (req, res) => {
-	    let testArray = [1, 2];
-	    res.send(testArray)
-	});
-
 	app.get('/image', (req, res) => {
 	    res.sendFile('libs/none.jpg', {root: __dirname })
 	});
+
+	app.get('/test', (req, res) => {
+	    res.send(jsonPaths)
+	});
+
+	var t;
+	for (var i = jsonPaths.length - 1; i >= 0; i--) {
+		// Из за того что експресс не желает видеть переменные цикла for, буду действовать в обход
+		t = i;
+		app.get(jsonPaths[t], (req, res) => {
+			console.log(jsonPaths[t]);
+		    res.sendFile(jsonPaths[t])
+		});
+	}
 
 	app.listen(8080, () => console.log('App listening on port 8080 \nhttp://localhost:8080'));
 }
